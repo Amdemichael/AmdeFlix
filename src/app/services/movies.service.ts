a079ff833a474f80794618cb17f1f52df1cef681
Amde <amdek2003@gmail.com>
2024-02-22 04:43:21 +0300

    Limit the numbers of movies to be returned

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -11,9 +11,9 @@ export class MoviesService {
   private apiKey = '52e3200582daea5c719f691e2e943b30';
   constructor(private http: HttpClient) {}
 
-  getMoviesByType(type: string) {
+  getMoviesByType(type: string, count = 20) {
     return this.http
       .get<MovieDto>(`${this.apiUrl}/movie/${type}?api_key=${this.apiKey}`)
-      .pipe(map((data) => data.results));
+      .pipe(map((data) => data.results.slice(0, count)));
   }
 }
